37e69d8fceac33cf8534992bc8799af4e4762458
originalop <rswami5579@gmail.com>
2025-03-18 09:45:18 +0530

    New file added

---
+++ Sem_3_Journal_1/ClassDemos/src/RunForLoop/NonThreadDemo.java
@@ -0,0 +1,11 @@
+package RunForLoop;
+
+public class NonThreadDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; System.out.println(i++))
+            ;
+        for (int i = 100; i < 201; System.out.println(i++))
+            ;
+    }
+
+}
