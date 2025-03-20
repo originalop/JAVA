adf57763e7035d619590362a4ed4c337aa7862df
originalop <rswami5579@gmail.com>
2025-03-21 00:10:38 +0530

    Added 3 cases which includes add, add(index, Value) and exit(), which terminate the program

--- Collection_Framework/src/LinkedListDemo.java
+++ Collection_Framework/src/LinkedListDemo.java
@@ -3,10 +3,35 @@ import java.util.*;
 public class LinkedListDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LinkedList<Integer> list=new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        int choice;
+        int insVal;
+        int index;
+        int output;
         while (true) {
             System.out.println("1. Add at the end");// Append at the last
             System.out.println("2. Add at specified index");// Add at the specified index
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter insert value: ");
+                    insVal = sc.nextInt();
+                    list.add(insVal);
+                    break;
+                case 2:
+                    System.out.println("Enter insert value: ");
+                    insVal = sc.nextInt();
+                    System.out.println("Enter index: ");
+                    index = sc.nextInt();
+                    list.add(index, insVal);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input");
+            }
 
         }
     }
