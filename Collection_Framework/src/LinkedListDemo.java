939108223b591250e74126b5771d5aaaf8ec06b1
originalop <rswami5579@gmail.com>
2025-03-21 00:00:25 +0530

    Added 2 lines for 2 cases

---
+++ Collection_Framework/src/LinkedListDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list=new LinkedList<>();
+        while (true) {
+            System.out.println("1. Add at the end");// Append at the last
+            System.out.println("2. Add at specified index");// Add at the specified index
+
+        }
+    }
+
+}
